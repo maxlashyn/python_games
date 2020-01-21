9461d09ea49538a08eb089a08a5e2eb6ed8e6683
max <maxim.lashyn@gmail.com>
2020-01-21 22:02:56 +0200

    урок 3 + task_1.py

--- lesson_3/task_1.py
+++ lesson_3/task_1.py
@@ -5,3 +5,22 @@
 результат сохраняется и используется как первый операнд следующей операции
 """
 
+available_operation = ('+', '-', '*', '/')
+a = int(input('enter number 1: '))
+while True:
+    c = input('операция: ')
+    if c in available_operation:
+        b = int(input('enter number 2: '))
+        if c == '+':
+            result = a + b
+        if c == '-':
+            result = a - b
+        if c == '*':
+            result = a * b
+        if c == '/':
+            result = a / b
+        print(result)
+        a = result
+    else:
+        print('недопустимая операция')
+
