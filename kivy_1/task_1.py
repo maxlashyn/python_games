f24417454b3cbfb4a4435ba51e75e02cb24e3ab0
Igor Lashyn <ilashyn@gmail.com>
2020-01-28 17:05:34 +0200

    отражение шарика от стенок

--- kivy_1/task_1.py
+++ kivy_1/task_1.py
@@ -1,16 +1,37 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Ellipse
+from kivy.clock import Clock
+from kivy.vector import Vector
+
+DX_SPEED = 10
+DY_SPEED = 10
+
 
 class MyWidget(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.dx = DX_SPEED
+        self.dy = DY_SPEED
         with self.canvas:
-            Ellipse(pos=(0,0), size=(20, 20))
+            self.ellipse = Ellipse(pos=(0, 0), size=(20, 20))
+
+    def update(self, dt):
+        pos = self.ellipse.pos
+        if 0 > pos[0] + self.dx or pos[0] + self.dx > self.right:
+            self.dx *= -1
+        if 0 > pos[1] + self.dy or pos[1] + self.dy > self.top:
+            self.dy *= -1
+
+        self.ellipse.pos = Vector(self.dx, self.dy) + self.ellipse.pos
+
 
 class MyApp(App):
     def build(self):
-        return MyWidget()
+        circle = MyWidget()
+        Clock.schedule_interval(circle.update, 1.0 / 60)
+        return circle
+
 
 if __name__ == '__main__':
     app = MyApp()
