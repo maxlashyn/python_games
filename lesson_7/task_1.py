ea17d521d09790cd82aabd44d3a860cc1425033f
Igor Lashyn <ilashyn@gmail.com>
2020-01-28 16:42:30 +0200

    Лекция 7 задание 1 (в процессе розработки)

--- lesson_7/task_1.py
+++ lesson_7/task_1.py
@@ -38,3 +38,96 @@ h - лечиться
 "Следующее сокровище находится выше-ниже-левее-правее на х шагов"
 
 """
+from random import randint
+
+MAP_WIDTH = 50
+MAP_HEIGHT = 20
+GRASS = ' '
+TREE = '1'
+STONE = 'D'
+TREASURE = 'X'
+LETTER = '@'
+MONSTER = '&'
+USER = 'H'
+
+MAX_TREES = 500
+MAX_STONES = 20
+MAX_TREASURES = 7
+MAX_LETTERS = MAX_TREASURES
+MAX_MONSTERS = 2
+
+
+def get_random_pos():
+    return randint(0, MAP_WIDTH), randint(0, MAP_HEIGHT)
+
+
+class Map:
+    def __init__(self):
+        self.field = []
+
+    def generate(self):
+        self.field = [
+            [GRASS for i in range(MAP_WIDTH + 1)] for j in range(MAP_HEIGHT + 1)
+        ]
+        for i in range(MAX_TREES):
+            x, y = get_random_pos()
+            self.field[y][x] = TREE
+        for i in range(MAX_STONES):
+            x, y = get_random_pos()
+            self.field[y][x] = STONE
+        for i in range(MAX_TREASURES):
+            x, y = get_random_pos()
+            while self.field[y][x] == TREASURE:
+                x, y = get_random_pos()
+            else:
+                self.field[y][x] = TREASURE
+        for i in range(MAX_LETTERS):
+            x, y = get_random_pos()
+            while self.field[y][x] in (LETTER, TREASURE):
+                x, y = get_random_pos()
+            else:
+                self.field[y][x] = LETTER
+
+    def show(self):
+        print(''.join(['-' for i in range(MAP_WIDTH)]))
+        for y in range(MAP_HEIGHT + 1):
+            print("".join(self.field[y]))
+        print(''.join(['-' for i in range(MAP_WIDTH)]))
+
+    def append(self, user):
+        x, y = MAP_WIDTH // 2, MAP_HEIGHT // 2
+        while self.field[y][x] in (LETTER, TREASURE):
+            x, y = get_random_pos()
+        else:
+            self.field[y][x] = USER
+            user.set_position(x, y)
+
+
+class Monster:
+    pass
+
+
+class User:
+    def __init__(self):
+        self.inventory = []
+        self.pos = (0, 0)
+
+    def turn(self):
+        c = input('Ваше действие [w, s, a, d, i, f, h, r, x]:')
+
+    def show_inventory(self):
+        print(f"В Вашем инвентаре {len(self.inventory)} сокровищ")
+
+    def set_position(self, x, y):
+        self.pos = (x, y)
+
+
+field = Map()
+field.generate()
+user = User()
+field.append(user)
+
+while True:
+    field.show()
+    user.show_inventory()
+    user.turn()
