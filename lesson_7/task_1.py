5afc7d61a6f63eb5d8a30045c0ddc473fe705ea1
max <maxim.lashyn@gmail.com>
2020-01-29 22:21:30 +0200

    урок 7

--- lesson_7/task_1.py
+++ lesson_7/task_1.py
@@ -3,6 +3,7 @@ Adventure game типа "Симулятор ходьбы"
 
 
 Плоский мир на котором могут быть объекты типа трава, дерево, камень, клад, инструкция, монстр.
+Присутствует туман войны размером 4, 4 от позиции юзера
 
 Игрок респается в средине карты.
 Игрок может идти по траве, может рубить дерево (на месте срубленного дерева остается трава), не может пройти камень,
@@ -25,7 +26,7 @@ h - лечиться
 если ходим и ход возможен:
 "вы идете по зеленой траве"
 если в клетке по ходу дерево:
-"вы уперлись лбом в дерево"
+"вы уперлись лбом в дерево"x
 >>> Ваше действие [w, s, a, d, i, f, h, r, x]:?
 
 если в клетке сокровище:
@@ -49,6 +50,8 @@ TREASURE = 'X'
 LETTER = '@'
 MONSTER = '&'
 USER = 'H'
+SMOG = '#'
+SMOG_RADIUS = 4
 
 MAX_TREES = 500
 MAX_STONES = 20
@@ -64,6 +67,7 @@ def get_random_pos():
 class Map:
     def __init__(self):
         self.field = []
+        self.user_pos = (0, 0)
 
     def generate(self):
         self.field = [
@@ -90,8 +94,9 @@ class Map:
 
     def show(self):
         print(''.join(['-' for i in range(MAP_WIDTH)]))
+        field_with_smog = self.get_field_with_smog()
         for y in range(MAP_HEIGHT + 1):
-            print("".join(self.field[y]))
+            print("".join(field_with_smog[y]))
         print(''.join(['-' for i in range(MAP_WIDTH)]))
 
     def append(self, user):
@@ -99,8 +104,35 @@ class Map:
         while self.field[y][x] in (LETTER, TREASURE):
             x, y = get_random_pos()
         else:
-            self.field[y][x] = USER
-            user.set_position(x, y)
+            self.set_user_position(user, x, y)
+
+    def get_objects(self, x, y):
+        if x < 0 or x > MAP_WIDTH:
+            return STONE
+        if y < 0 or y > MAP_HEIGHT:
+            return STONE
+        return self.field[y][x]
+
+    def move_to(self, user, x, y):
+        user_pos = user.get_position()
+        self.field[user_pos[1]][user_pos[0]] = GRASS
+        self.set_user_position(user, x, y)
+
+    def set_user_position(self, user, x, y):
+        self.user_pos = (x, y)
+        self.field[y][x] = USER
+        user.set_position(x, y)
+
+    def get_field_with_smog(self):
+        field_with_smog = []
+        for y in range(MAP_HEIGHT + 1):
+            field_with_smog.append(self.field[y].copy())
+            for x in range(MAP_WIDTH + 1):
+                if x < self.user_pos[0] - SMOG_RADIUS or x > self.user_pos[0] + SMOG_RADIUS:
+                    field_with_smog[y][x] = SMOG
+                elif y < self.user_pos[1] - SMOG_RADIUS or y > self.user_pos[1] + SMOG_RADIUS:
+                    field_with_smog[y][x] = SMOG
+        return field_with_smog
 
 
 class Monster:
@@ -111,15 +143,47 @@ class User:
     def __init__(self):
         self.inventory = []
         self.pos = (0, 0)
+        self.direction = 'w'
+        self.message = ''
 
-    def turn(self):
+    def turn(self, field):
         c = input('Ваше действие [w, s, a, d, i, f, h, r, x]:')
+        if c in ('w', 's', 'a', 'd'):
+            self.direction = c
+            new_pos = (0, 0)
+            if c == 'w':
+                new_pos = (self.pos[0], self.pos[1] - 1)
+            if c == 's':
+                new_pos = (self.pos[0], self.pos[1] + 1)
+            if c == 'a':
+                new_pos = (self.pos[0] - 1, self.pos[1])
+            if c == 'd':
+                new_pos = (self.pos[0] + 1, self.pos[1])
+            objects = field.get_objects(new_pos[0], new_pos[1])
+            if objects == STONE:
+                self.message = 'Мы уперлись в камень!'
+                return
+            if objects == TREE:
+                self.message = 'Мы уперлись в дерево!'
+                return
+            if objects in [TREASURE, LETTER]:
+                self.message = 'Мы нашли сундук или письмо'
+                return
+            self.message = 'Мы пошли дальше'
+            field.move_to(user, new_pos[0], new_pos[1])
 
     def show_inventory(self):
         print(f"В Вашем инвентаре {len(self.inventory)} сокровищ")
 
     def set_position(self, x, y):
         self.pos = (x, y)
+
+    def get_position(self):
+        return self.pos
+
+    def show_message(self):
+        if len(self.message) > 0:
+            print(self.message)
 
 
 field = Map()
@@ -129,5 +193,6 @@ field.append(user)
 
 while True:
     field.show()
+    user.show_message()
     user.show_inventory()
-    user.turn()
+    user.turn(field)
