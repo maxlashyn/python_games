8daabe531728f2a91da025160f127087d6787cb4
max <maxim.lashyn@gmail.com>
2020-02-04 22:21:24 +0200

    tennis

--- kivy_2/tennis.py
+++ kivy_2/tennis.py
@@ -25,9 +25,9 @@ from kivy.clock import Clock
 from kivy.vector import Vector
 from random import choice
 
-SPEED = 1
-DELTA_SPEED = 5
-RACKET_SPEED = 20
+SPEED = 2
+DELTA_SPEED = 1
+RACKET_SPEED = 40
 SLEEP_BEFORE_RUN = 500
 
 
@@ -46,8 +46,13 @@ class Field(Widget):
             self.center_line = Rectangle(pos=(Window.size[0] // 2, 0), size=(5, Window.size[1]))
             self.left_label = Label(pos_hint=(0.1, 0.1), text=str(self.left_player_score))
             self.left_racket = Rectangle(pos=(20, Window.size[1] // 2 - 50), size=(5, 100))
+
+            self.right_label = Label(pos_hint=(0.2, 0.2), text=str(self.right_player_score))
+            self.right_racket = Rectangle(pos=(Window.size[0] - 20, Window.size[1] // 2 - 50), size=(5, 100))
+
             self.ball = Ellipse(pos=(Window.size[0] // 2 - 15, Window.size[1] // 2 - 15), size=(30, 30))
-            self.circle = Line(circle=(Window.size[0] // 2, Window.size[1] // 2, min(self.width, self.height) / 2, 0, 360))
+            self.circle = Line(
+                circle=(Window.size[0] // 2, Window.size[1] // 2, 100, 0, 360))
 
     def _on_keyboard_close(self):
         self._keyboard.unbind(on_key_down=self._on_key_down)
@@ -58,14 +63,24 @@ class Field(Widget):
             self.left_racket.pos = Vector(0, RACKET_SPEED) + self.left_racket.pos
         if text == 's' and self.left_racket.pos[1] > 0:
             self.left_racket.pos = Vector(0, -RACKET_SPEED) + self.left_racket.pos
+        if text == 'o' and self.right_racket.pos[1] + self.right_racket.size[1] < self.height:
+            self.right_racket.pos = Vector(0, RACKET_SPEED) + self.right_racket.pos
+        if text == 'l' and self.right_racket.pos[1] > 0:
+            self.right_racket.pos = Vector(0, -RACKET_SPEED) + self.right_racket.pos
 
     def check_collision_left(self, ball, racket):
         return ball.pos[0] < racket.pos[0] + racket.size[0] \
                and racket.pos[1] <= ball.pos[1] + ball.size[1] // 2 <= racket.pos[1] + racket.size[1]
 
+    def check_collision_right(self, ball, racket):
+        return ball.pos[0] + ball.size[0] > racket.pos[0] \
+               and racket.pos[1] <= ball.pos[1] + ball.size[1] // 2 <= racket.pos[1] + racket.size[1]
+
     def update(self, dt):
         self.center_line.pos = (Window.size[0] // 2, 0)
         self.center_line.size = (5, Window.size[1])
+        self.right_racket.pos = (Window.size[0] - 20, self.right_racket.pos[1])
+        self.circle.circle = (Window.size[0] // 2, Window.size[1] // 2, 100, 0, 360)
 
         if self.run < SLEEP_BEFORE_RUN:
             self.run += 1
@@ -74,17 +89,21 @@ class Field(Widget):
         if self.height < ball_pos[1] + self.ball.size[1] + self.dy or ball_pos[1] + self.dy < 0:
             self.dy *= -1
 
-        # отскок от правой стороны
-        if self.width < ball_pos[0] + self.ball.size[0] + self.dx:
-            self.dx *= -1
-
         if self.check_collision_left(self.ball, self.left_racket):
             self.dx = abs(self.dx) + DELTA_SPEED
             self.dy = (abs(self.dy) + DELTA_SPEED) * (1 if self.dy > 0 else -1)
         elif ball_pos[0] + self.dx < 0:
             self.init_ball()
             self.right_player_score += 1
-            self.left_label.text = str(self.right_player_score)
+            self.right_label.text = str(self.right_player_score)
+
+        if self.check_collision_right(self.ball, self.right_racket):
+            self.dx = -(abs(self.dx) + DELTA_SPEED)
+            self.dy = (abs(self.dy) + DELTA_SPEED) * (1 if self.dy > 0 else -1)
+        elif ball_pos[0] + self.ball.size[0] + self.dx > self.right:
+            self.init_ball()
+            self.left_player_score += 1
+            self.left_label.text = str(self.left_player_score)
 
         self.ball.pos = Vector(self.dx, self.dy) + self.ball.pos
 
